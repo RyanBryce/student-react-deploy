fc72ad2438aee1161dabccb6f7edfd68ccbb3517
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-06-19 21:22:40 -0600

    test react heroku 1

---
+++ client/build/precache-manifest.0e2c9135b829b3ed8af127f36044fdef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76262ee4d9300f5005c4",
+    "url": "/static/css/main.c48f9978.chunk.css"
+  },
+  {
+    "revision": "76262ee4d9300f5005c4",
+    "url": "/static/js/main.0c9b6b8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "80ed83637b961f67d4209d73d4f34931",
+    "url": "/index.html"
+  }
+];
